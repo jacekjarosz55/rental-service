c8c1dd1f92d6e2d7db01980496b5ac3a55eff03e
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-06-06 16:27:49 +0200

    documentation for rent.h

--- rent.h
+++ rent.h
@@ -1,3 +1,6 @@
+/* @file rent.h
+ * @brief Rent data structure definition.
+ */
 #ifndef RENT_H
 #define RENT_H
 
@@ -7,25 +10,64 @@
 #include "clientlist.h"
 #include <stdbool.h>
 
+/**
+ * @struct Rent
+ * @brief Represents a rental transaction in the system.
+ */
 struct Rent {
-  unsigned id;
-  unsigned car_id;
-  unsigned client_id;
-  bool finished;
-  char *date_start;
-  char *date_end;
+  unsigned id;          /**< Rent identifier. */
+  unsigned car_id;      /**< Identifier of the rented car. */
+  unsigned client_id;   /**< Identifier of the client. */
+  bool finished;        /**< Status of the rent (finished or not). */
+  char *date_start;     /**< Start date of the rent. */
+  char *date_end;       /**< End date of the rent. */
 };
+
 typedef struct Rent Rent;
 
-
-
-
+/**
+ * @brief Creates a new rent data instance.
+ * 
+ * @param id Rent identifier.
+ * @param car_id Identifier of the rented car.
+ * @param client_id Identifier of the client.
+ * @param date_start Start date of the rent.
+ * @param date_end End date of the rent.
+ * @param finished Status of the rent (finished or not).
+ * @return Pointer to the newly created Rent instance.
+ */
 Rent *make_rent_data(unsigned id, unsigned car_id, unsigned client_id, char *date_start, char *date_end, bool finished);
+
+/**
+ * @brief Frees the memory allocated for a Rent instance.
+ * 
+ * @param rent Pointer to the Rent instance to be freed.
+ */
 void free_rent_data(Rent *rent);
 
+/**
+ * @brief Marks a rent as finished.
+ * 
+ * @param rent Pointer to the Rent instance to be finished.
+ */
 void finish_rent(Rent *rent);
 
+/**
+ * @brief Fetches the car associated with a rent.
+ * 
+ * @param rent Pointer to the Rent instance.
+ * @param cars Pointer to the list of cars.
+ * @return Pointer to the associated Car instance.
+ */
 Car *fetch_car(Rent *rent, CarNode *cars);
+
+/**
+ * @brief Fetches the client associated with a rent.
+ * 
+ * @param rent Pointer to the Rent instance.
+ * @param clients Pointer to the list of clients.
+ * @return Pointer to the associated Client instance.
+ */
 Client *fetch_client(Rent *rent, ClientNode *clients);
 
-#endif
+#endif // RENT_H
