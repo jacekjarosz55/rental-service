5ea09a194feb2f96f94e586513c008ab8b37c4c3
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-06-06 16:26:03 +0200

    car.h documentation

--- car.h
+++ car.h
@@ -1,22 +1,43 @@
+/** @file car.h 
+ *  @brief Car data structure definition.
+ */
+
 #ifndef CAR_H
 #define CAR_H
 
+/**
+ * @struct Car
+ * @brief Represents a car in the system.
+ */
 struct Car {
-  unsigned id;
-  char *model;
-  char *make;
-  unsigned year;
-  unsigned cost;
-  unsigned km_driven;
+  unsigned id;          /**< Car identifier. */
+  char *model;          /**< Model of the car. */
+  char *make;           /**< Make of the car. */
+  unsigned year;        /**< Manufacture year of the car. */
+  unsigned cost;        /**< Cost of the car (per kilometer). */
+  unsigned km_driven;   /**< Car mileage (in kilometers) */
 };
+
 typedef struct Car Car;
 
+/**
+ * @brief Creates a new car data instance.
+ * 
+ * @param id Car identifier.
+ * @param make Make of the car.
+ * @param model Model of the car.
+ * @param year Manufacture year of the car.
+ * @param cost Cost of the car (per kilometer).
+ * @param km_driven Car mileage (in kilometers)
+ * @return Pointer to the newly created Car instance.
+ */
 Car *make_car_data(unsigned id, char *make, char *model, unsigned year, unsigned cost, unsigned km_driven);
+
+/**
+ * @brief Frees the memory allocated for a Car instance.
+ * 
+ * @param car Pointer to the Car instance to be freed.
+ */
 void free_car_data(Car *car);
-typedef struct Car Car;
 
-
-
-
-
-#endif
+#endif // CAR_H
