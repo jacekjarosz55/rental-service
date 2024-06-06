644b503fa833e673f4040bade06f3e92a145b292
Jacek Jarosz <jacekjarosz55@gmail.com>
2024-06-06 16:04:29 +0200

    added documentation for util.h

--- util.h
+++ util.h
@@ -1,6 +1,13 @@
+/** @file 
+ *  @brief Utility functions */
+
 #ifndef UTIL_H
 #define UTIL_H
 
+/** Copy the string and allocate it on the heap.
+  * @param str The string to be copied (can be const).
+  * @returns a pointer to the copy of the string.
+*/
 char *copied_string(char *str);
 
 
